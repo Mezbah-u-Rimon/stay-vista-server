c7022e21d51c78a7820752da5ec16fbfdf770d58
Muzbah Uddin <rimonmr444@gmail.com>
2024-01-20 23:53:51 +0600

    get all bookings for guest and host

--- index.js
+++ index.js
@@ -171,9 +171,31 @@ async function run() {
       res.send(result)
     })
 
+    //get all bookings for guest
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.send([])
+      }
+      const query = { 'guest.email': email }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    //get all bookings for host
+    app.get('/bookings/host', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.send([])
+      }
+      const query = { host: email }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    // await client.db('admin').command({ ping: 1 })
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
